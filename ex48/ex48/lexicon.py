aad5665e671460133454494236bfe66214508556
Eric Brooke <ericbrooke@mac.com>
2018-04-08 17:48:27 -0500

    ex48 incomplete - add error

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -8,17 +8,29 @@ def scan(text):
 
         #  directions
         if word in ('north', 'south', 'west', 'east'):
-            tuple = ("direction", word)
-            processed_tuples.append(tuple)
+            processed_tuples.append(("direction", word))
 
         #  verbs
         if word in ('go', 'kill', 'eat'):
-                tuple = ("verb", word)
-                processed_tuples.append(tuple)
+                processed_tuples.append(("verb", word))
 
         #  stop
         if word in ('the', 'in', 'of'):
-                tuple = ("stop", word)
-                processed_tuples.append(tuple)
+                processed_tuples.append(("stop", word))
+
+        #  noun
+        if word in ('bear', 'princess'):
+                processed_tuples.append(("noun", word))
+
+        #  number
+        if convert_numbers(word):
+                processed_tuples.append(("number", int(word)))
 
     return processed_tuples
+
+
+def convert_numbers(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
